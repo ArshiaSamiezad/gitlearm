535ef21c5d1a0372f9034575ff809253e1cb2b4f
ArshiaSamiezad <arshiasamiezad@gmail.com>
2024-06-01 15:31:13 +0330

    fixed

--- Main.java
+++ Main.java
@@ -5,31 +5,32 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         GetEnterPressed getEnterPressed = new GetEnterPressed();
         getEnterPressed.start();
-        while(true){
+        while (true) {
             LocalTime localTime = LocalTime.now();
-            System.out.println("Time: "+localTime);
+            System.out.println("Time: " + localTime);
             System.out.println("Press enter to exit");
 
             Thread.sleep(500);
             clearScreen();
 
-            if(!getEnterPressed.isAlive()){
+            if (!getEnterPressed.isAlive()) {
                 break;
             }
         }
     }
 
-    public static void clearScreen(){
+    public static void clearScreen() {
         System.out.println("\033[H\033[2J");
         System.out.flush();
     }
 }
 
-class GetEnterPressed extends Thread{
+class GetEnterPressed extends Thread {
     @Override
-    public void run(){
+    public void run() {
         Scanner scanner = new Scanner(System.in);
         String nextLine = scanner.nextLine();
+        //aaa
         // kose babat
     }
 }
