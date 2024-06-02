bec2c5af9f893d8c1dbf432027aa6a9137f75f31
ArshiaSamiezad <arshiasamiezad@gmail.com>
2024-06-02 10:44:26 +0330

    cemit

--- Ex2.java
+++ Ex2.java
@@ -2,13 +2,15 @@ public class Ex2 {
     public static class threadTest implements Runnable {
         @Override
         public void run() {
-            System.out.println("Thread is running");
+            System.out.println("Thread is running gay");
         }
+        // b
     }
 
     public static void main(String[] args) {
         Thread thread = new Thread(new threadTest());
-        Thread thread2 = null;
+        Thread thread2 = new Thread(new threadTest());
         thread.start();
+        //this is sam
     }
 }
